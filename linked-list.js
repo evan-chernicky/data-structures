95bdb1e7ae6fe8d5d89b37532a1d19b96abb0e30
evan-chernicky <echernicky@gmail.com>
2022-07-12 16:08:58 -0400

    added git

--- linked-list.js
+++ linked-list.js
@@ -70,9 +70,18 @@ class LinkedList {
         }
         return temp
     }    
+
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined
+        let temp = this.head
+
+        for (let i = 0; i < index; i++) {
+            temp = temp.next
+        }
+        return temp
+    }
 }
     
-}
 
 let myLinkedList = new LinkedList(4)
 
