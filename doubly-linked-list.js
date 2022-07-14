c4d013c02798399797f080a08c52b0a1ebc67029
evan-chernicky <echernicky@gmail.com>
2022-07-14 09:26:17 -0400

    push

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -13,6 +13,22 @@ class DoublyLinkedList {
         this.tail = newNode
         this.length = 1
     }
+
+    push(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        }
+        else {
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+
+        return this
+    }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(7)
