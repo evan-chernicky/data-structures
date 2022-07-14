a93dd67663e6b679a582cbdcfc10f347c3678904
evan-chernicky <echernicky@gmail.com>
2022-07-14 09:41:46 -0400

    pop for doublylinked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -29,6 +29,27 @@ class DoublyLinkedList {
 
         return this
     }
+
+    pop() {
+        if (this.length === 0) return undefined
+        
+        let temp = this.tail
+        
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        }
+        else {
+            this.tail = this.tail.prev
+            this.tail.next = null
+            temp.prev = null   
+        }
+
+        this.length--
+
+        return temp        
+        
+    }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(7)
